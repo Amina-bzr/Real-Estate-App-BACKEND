a2affc22ed30049b2236327fcba65d6a0c4fe67d
Amina-bzr <ka_bouzar@esi.dz>
2023-02-04 23:12:15 +0000

    sel

--- realestate/tests.py
+++ realestate/tests.py
@@ -4,7 +4,12 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
-
+# selenium
+from django.test import LiveServerTestCase
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 
 # tests Annonce
 class AnnonceTests(APITestCase):
@@ -53,3 +58,87 @@ class OffreTests(APITestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+#test selenium
+class PlayerFormTest(LiveServerTestCase):
+
+    def testform(self):
+        driver = webdriver.Chrome()
+        driver.get("http://localhost:5173")
+        main_page = driver.current_window_handle
+
+        # selectionner le boutton du login
+        time.sleep(5)
+        btn_login = driver.find_element(By.ID, "googlesign")
+        btn_login.click()
+
+        # s'authentifier
+
+        for handle in driver.window_handles:
+            if handle != main_page:
+                login_page = handle
+
+        driver.switch_to.window(login_page)
+
+        time.sleep(5)
+        email_field = driver.find_element(By.ID, "identifierId")
+        email_field.send_keys("trialcomptetrial@gmail.com")
+
+        next_button = driver.find_element(By.ID, "identifierNext")
+        next_button.click()
+        time.sleep(3)
+        password_field = driver.find_element(By.TAG_NAME, "input")
+        password_field.send_keys("appart1234")
+
+        signin_button = driver.find_element(By.ID, "passwordNext")
+        signin_button.click()
+        time.sleep(3)
+        driver.switch_to.window(main_page)
+        # publier
+        time.sleep(10)
+        ''' btns_compte = driver.find_element(
+            By.CLASS_NAME, "fFW7wc-ibnC6b-sM5MNb TAKBxb")
+        btns_compte[0].click() '''
+
+        publier_btn = driver.find_element(By.ID, "publier")
+        publier_btn.click()
+
+        time.sleep(5)
+        input_field = driver.find_element(By.ID, "titre")
+        input_field.send_keys("nouvelle annonce")
+
+        input_field1 = driver.find_element(By.ID, "Surface")
+        input_field1.send_keys("2000")
+
+        input_field2 = driver.find_element(By.ID, "Description")
+        input_field2.send_keys("annonce pour vente d'une maison")
+
+        input_field3 = driver.find_element(By.ID, "Commune")
+        input_field3.send_keys("babezouar")
+
+        input_field4 = driver.find_element(By.ID, "Prix")
+        input_field4.send_keys("5000")
+
+        input_field5 = driver.find_element(By.ID, "Addresse")
+        input_field5.send_keys("street5")
+
+        select_element = Select(driver.find_element(By.ID, "Categorie"))
+        select_element.select_by_visible_text("Vente")
+
+        select_element2 = Select(driver.find_element(By.ID, "Type"))
+        select_element2.select_by_visible_text("Maison")
+
+        select_element3 = Select(driver.find_element(By.ID, "Wilaya"))
+        select_element3.select_by_visible_text("Alger")
+
+        image_uploader = driver.find_element("name", "images")
+        image_uploader.send_keys(
+            "/home/amina/developement/api-realestate-app/media/userPhotos/profilePic.jpg")
+        image_uploader.send_keys(
+            "/home/amina/developement/api-realestate-app/media/userPhotos/profilePic1.jpg")
+
+        button = driver.find_element(By.CLASS_NAME, "upload-btn")
+        button.click()
+        button2 = driver.find_element(By.ID, "pub")
+        button2.click()
+
+        driver.quit()
